77667da38b42ea89b49cff488b92bc4754fff141
patel9619ali <patel9619ali@gmail.com>
2025-09-30 17:35:05 +0530

    breadcrumb assets images

--- components/BannerComponent/HeroBanner.tsx
+++ components/BannerComponent/HeroBanner.tsx
@@ -2,6 +2,6 @@ import Image from "next/image";
 
 export default function HeroBanner() {
   return (
-    <Image src={`/assets/images/banner_festive.webp`} className="w-full h-full" alt={`asd`} width={1440} height={333}/>
+    <Image src={`${process.env.NEXT_PUBLIC_APP_BASE_URL}assets/images/banner_festive.webp`} className="w-full h-full" alt={`asd`} width={1440} height={333}/>
   );
 }
--- components/Breadcrumb/Breadcrumb.tsx
+++ components/Breadcrumb/Breadcrumb.tsx
@@ -18,7 +18,8 @@ export default function BreadcrumbWrapper({className}:Props) {
       <BreadcrumbList className='max-w-7xl mx-auto md:py-4 py-3'>
         <BreadcrumbItem>
           <BreadcrumbLink href="https://www.hdfcbank.com/">
-            <Home className="size-4" stroke='#004C8F' />
+            {/* <Home className="size-5" stroke='#fff' fill='#004c8f' /> */}
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-house-icon lucide-house"><path d="M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8"/><path d="M3 10a2 2 0 0 1 .709-1.528l7-6a2 2 0 0 1 2.582 0l7 6A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"/></svg>
           </BreadcrumbLink>
         </BreadcrumbItem>
           <BreadcrumbSeparator className={`${styles.seperator} text-[#004C8F]`} />
