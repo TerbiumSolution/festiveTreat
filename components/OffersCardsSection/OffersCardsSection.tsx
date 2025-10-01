7e8dd9b782df2310a3ced3a2c91e29e5591951e0
Isa Sayed <sayedisa96@gmail.com>
2025-10-01 22:01:38 +0530

    hide get offer button

--- components/OffersCardsSection/OffersCardsSection.tsx
+++ components/OffersCardsSection/OffersCardsSection.tsx
@@ -84,95 +84,97 @@ export default function OffersCardsSection({ context, deals }: { context: Compon
 
    return (
       <>
-      <section className="px-4 py-6">
-         <div className="max-w-7xl mx-auto">
-            <h2 className="text-2xl font-semibold mb-6 pb-2 inline-block border-b">{getH1(layout, title, category?.categoryContent?.h1, subcategory?.subcategoryContent?.h1, merchant?.merchantContent?.h1, category?.name, subcategory?.name, merchant?.name, state?.name, city?.name)}</h2>
-            <div className={`grid ${visibleCards.length > 1 ? 'grid-cols-1 lg:grid-cols-2 gap-6' : 'grid-cols-1'}`}>
-               {visibleCards.map((offer, index) => (
-                  <Card key={`${offer.name}-${index}`} className="rounded-2xl shadow-md p-0">
-                     <CardContent className={visibleCards.length > 1 ? 'h-full p-4' : 'md:p-8 p-6'}>
-                        <div className={`${visibleCards.length > 1 ? ' gap-4' : 'md:gap-8 gap-6' } flex flex-col h-full sm:flex-row`}>
-                           <div className="sm:w-[40%] w-full">
-                              <Image
-                                 src={offer.image ? offer.image.url : `${process.env.NEXT_PUBLIC_APP_BASE_URL}${offer.image}`}
-                                 alt={offer.name}
-                                 width={200}
-                                 height={200}
-                                 className="rounded-xl object-cover w-full h-full"
-                                 style={{ border: '1px solid #F5F5F5' }}
-                              />
-                           </div>
-                           <div className="flex flex-col sm:w-[60%] w-full flex-auto  justify-between">
-                              <div >
-                                 <div className="flex justify-between">
-                                    <div>
-                                       <p className={`${visibleCards.length > 1 ? 'text-sm mb-2' : 'text-xl mb-4' } font-bold`}>Merchant Name</p>
-                                       <Image
-                                          src={offer.subcategoryMerchants[0]?.merchant?.image?.url || `${process.env.NEXT_PUBLIC_APP_BASE_URL}assets/images/bose_logo.jpg`}
-                                          alt={offer.name}
-                                          width={100}
-                                          height={100}
-                                       />
-                                    </div>
-                                    <div>
-                                       <p className={`${visibleCards.length > 1 ? 'text-sm mb-2' : 'text-xl mb-4'} font-bold `}>End Date</p>
-                                       <p className={`${visibleCards.length > 1 ? 'text-[12px]' : 'text-xl'} font-medium text-[#6B6B6B]`}>{offer.endDate}</p>
-                                    </div>
-                                 </div>
-                                 {offer.details && (
-                                    offer.details.map((detail, index) => (
-                                       <div className={visibleCards.length > 1 ? 'mt-3' : 'mt-6'} key={`${detail.content}-${index}`}>
-                                          <p className={`${visibleCards.length > 1 ? 'text-sm mb-2' : 'text-lg mb-6'} font-bold `}>Offer Details</p>
-                                          <ul>
-                                             <li className="flex gap-1">
-                                                <CircleCheck color="#fff" fill="#00bc19" size={20} className={visibleCards.length > 1 ? 'w-[6%] mt-[-1px]' : 'w-[4%] mt-[3px]'}/>
-                                                <span className={`${visibleCards.length > 1 ? 'w-[94%] text-[12px]' : 'w-[96%] md:text-[20px] text-[16px]'} text-[#6B6B6B] font-semibold`} dangerouslySetInnerHTML={{ __html: detail.content }}></span>
-                                             </li>
-                                          </ul>
+         <section className="px-4 py-6">
+            <div className="max-w-7xl mx-auto">
+               <h2 className="text-2xl font-semibold mb-6 pb-2 inline-block border-b">{getH1(layout, title, category?.categoryContent?.h1, subcategory?.subcategoryContent?.h1, merchant?.merchantContent?.h1, category?.name, subcategory?.name, merchant?.name, state?.name, city?.name)}</h2>
+               <div className={`grid ${visibleCards.length > 1 ? 'grid-cols-1 lg:grid-cols-2 gap-6' : 'grid-cols-1'}`}>
+                  {visibleCards.map((offer, index) => (
+                     <Card key={`${offer.name}-${index}`} className="rounded-2xl shadow-md p-0">
+                        <CardContent className={visibleCards.length > 1 ? 'h-full p-4' : 'md:p-8 p-6'}>
+                           <div className={`${visibleCards.length > 1 ? ' gap-4' : 'md:gap-8 gap-6'} flex flex-col h-full sm:flex-row`}>
+                              <div className="sm:w-[40%] w-full">
+                                 <Image
+                                    src={offer.image ? offer.image.url : `${process.env.NEXT_PUBLIC_APP_BASE_URL}${offer.image}`}
+                                    alt={offer.name}
+                                    width={200}
+                                    height={200}
+                                    className="rounded-xl object-cover w-full h-full"
+                                    style={{ border: '1px solid #F5F5F5' }}
+                                 />
+                              </div>
+                              <div className="flex flex-col sm:w-[60%] w-full flex-auto  justify-between">
+                                 <div >
+                                    <div className="flex justify-between">
+                                       <div>
+                                          <p className={`${visibleCards.length > 1 ? 'text-sm mb-2' : 'text-xl mb-4'} font-bold`}>Merchant Name</p>
+                                          <Image
+                                             src={offer.subcategoryMerchants[0]?.merchant?.image?.url || `${process.env.NEXT_PUBLIC_APP_BASE_URL}assets/images/bose_logo.jpg`}
+                                             alt={offer.name}
+                                             width={100}
+                                             height={100}
+                                          />
                                        </div>
-                                    ))
-                                 )}
-                              </div>
-                              <div className="flex justify-end gap-3 mt-4">
-                                 <div className="group inline-flex items-center justify-center gap-2 bg-transparent border border-[#004c8f] rounded-lg cursor-pointer px-[12px] py-2 transition-all duration-300 ease-in-out hover:bg-[#004c8f] hover:text-[#fff] relative">
-                                    <Link
-                                       href={`${process.env.NEXT_PUBLIC_APP_BASE_URL}${offer.subcategoryMerchants[0]?.subcategory?.slug}/${offer.subcategoryMerchants[0]?.merchant?.slug}`}
-                                       className={`text-sm whitespace-nowrap !bg-[transparent] !p-0 hover:cursor-pointer font-semibold before:content-[''] before:absolute before:inset-0`}
-                                    >
-                                       Get Offer
-                                    </Link>
+                                       <div>
+                                          <p className={`${visibleCards.length > 1 ? 'text-sm mb-2' : 'text-xl mb-4'} font-bold `}>End Date</p>
+                                          <p className={`${visibleCards.length > 1 ? 'text-[12px]' : 'text-xl'} font-medium text-[#6B6B6B]`}>{offer.endDate}</p>
+                                       </div>
+                                    </div>
+                                    {offer.details && (
+                                       offer.details.map((detail, index) => (
+                                          <div className={visibleCards.length > 1 ? 'mt-3' : 'mt-6'} key={`${detail.content}-${index}`}>
+                                             <p className={`${visibleCards.length > 1 ? 'text-sm mb-2' : 'text-lg mb-6'} font-bold `}>Offer Details</p>
+                                             <ul>
+                                                <li className="flex gap-1">
+                                                   <CircleCheck color="#fff" fill="#00bc19" size={20} className={visibleCards.length > 1 ? 'w-[6%] mt-[-1px]' : 'w-[4%] mt-[3px]'} />
+                                                   <span className={`${visibleCards.length > 1 ? 'w-[94%] text-[12px]' : 'w-[96%] md:text-[20px] text-[16px]'} text-[#6B6B6B] font-semibold`} dangerouslySetInnerHTML={{ __html: detail.content }}></span>
+                                                </li>
+                                             </ul>
+                                          </div>
+                                       ))
+                                    )}
                                  </div>
-                                 <div className="group inline-flex items-center justify-center gap-2 bg-[#004c8f] border border-[#004c8f] text-white rounded-lg cursor-pointer px-[12px] py-2 transition-all duration-300 ease-in-out hover:bg-transparent hover:text-[#292929] relative">
-                                    <Link
-                                       href=""
-                                       className={`text-sm whitespace-nowrap !bg-[transparent] !p-0 hover:cursor-pointer font-semibold before:content-[''] before:absolute before:inset-0`}
-                                    >
-                                       Apply Now
-                                    </Link>
+                                 <div className="flex justify-end gap-3 mt-4">
+                                    {visibleCards.length > 1 && (
+                                       <div className="group inline-flex items-center justify-center gap-2 bg-transparent border border-[#004c8f] rounded-lg cursor-pointer px-[12px] py-2 transition-all duration-300 ease-in-out hover:bg-[#004c8f] hover:text-[#fff] relative">
+                                         <Link
+                                           href={`${process.env.NEXT_PUBLIC_APP_BASE_URL}${offer.subcategoryMerchants[0]?.subcategory?.slug}/${offer.subcategoryMerchants[0]?.merchant?.slug}`}
+                                           className={`text-sm whitespace-nowrap !bg-[transparent] !p-0 hover:cursor-pointer font-semibold before:content-[''] before:absolute before:inset-0`}
+                                         >
+                                           Get Offer
+                                         </Link>
+                                       </div>
+                                    )}
+                                    <div className="group inline-flex items-center justify-center gap-2 bg-[#004c8f] border border-[#004c8f] text-white rounded-lg cursor-pointer px-[12px] py-2 transition-all duration-300 ease-in-out hover:bg-transparent hover:text-[#292929] relative">
+                                       <Link
+                                          href=""
+                                          className={`text-sm whitespace-nowrap !bg-[transparent] !p-0 hover:cursor-pointer font-semibold before:content-[''] before:absolute before:inset-0`}
+                                       >
+                                          Apply Now
+                                       </Link>
+                                    </div>
                                  </div>
                               </div>
                            </div>
-                        </div>
-                     </CardContent>
-                  </Card>
-               ))}
-            </div>
-         </div>
-         {
-            shouldShowToggle && (
-               <div className="text-center">
-                  <div className="group inline-flex items-center justify-center gap-2 bg-[#004c8f] border border-[#004c8f] text-white rounded-lg cursor-pointer px-[12px] py-[13px]  transition-all duration-300 ease-in-out hover:bg-transparent hover:text-[#004c8f] mt-6"
-                     onClick={() => setShowAll(prev => !prev)}>
-                     <Button
-                        className={`!bg-[transparent] !p-0 !text-inherit !font-inherit !h-auto hover:cursor-pointer hover:!bg-[transparent] hover:!text-inherit !shadow-none font-semibold`}>
-                        {showAll ? 'View Less' : 'View More'} <ChevronRight />
-                     </Button>
-                  </div>
+                        </CardContent>
+                     </Card>
+                  ))}
                </div>
-            )
-         }
-      </section >
-      <GeneralInformation/>
+            </div>
+            {
+               shouldShowToggle && (
+                  <div className="text-center">
+                     <div className="group inline-flex items-center justify-center gap-2 bg-[#004c8f] border border-[#004c8f] text-white rounded-lg cursor-pointer px-[12px] py-[13px]  transition-all duration-300 ease-in-out hover:bg-transparent hover:text-[#004c8f] mt-6"
+                        onClick={() => setShowAll(prev => !prev)}>
+                        <Button
+                           className={`!bg-[transparent] !p-0 !text-inherit !font-inherit !h-auto hover:cursor-pointer hover:!bg-[transparent] hover:!text-inherit !shadow-none font-semibold`}>
+                           {showAll ? 'View Less' : 'View More'} <ChevronRight />
+                        </Button>
+                     </div>
+                  </div>
+               )
+            }
+         </section >
+         <GeneralInformation />
       </>
    )
 }
