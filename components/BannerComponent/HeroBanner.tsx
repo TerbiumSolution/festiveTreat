e160ac98f2e92243bc5b9117f3c8e2bda6ad0be4
patel9619ali <patel9619ali@gmail.com>
2025-10-01 19:36:42 +0530

    HeroBanner mobile

--- components/BannerComponent/HeroBanner.tsx
+++ components/BannerComponent/HeroBanner.tsx
@@ -2,6 +2,9 @@ import Image from "next/image";
 
 export default function HeroBanner() {
   return (
-    <Image src={`${process.env.NEXT_PUBLIC_APP_BASE_URL}assets/images/banner_festive.webp`} className="w-full h-full" alt={`asd`} width={1440} height={333}/>
+    <>
+    <Image src={`${process.env.NEXT_PUBLIC_APP_BASE_URL}assets/images/banner_festive.webp`} className="w-full h-full sm:block hidden" alt={`asd`} width={1440} height={333}/>
+    <Image src={`${process.env.NEXT_PUBLIC_APP_BASE_URL}assets/images/mobile_festive_banner.jpg`} className="w-full h-full sm:hidden block" alt={`asd`} width={425} height={425}/>
+    </>
   );
 }
