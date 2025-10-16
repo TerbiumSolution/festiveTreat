14a7daed481e10456ef0d7b34e0b900348676543
Mohammad-Nonsola <mohammadnonsola1@gmail.com>
2025-10-16 20:23:03 +0530

    page error fixed

--- app/[...slug]/page.tsx
+++ app/[...slug]/page.tsx
@@ -78,6 +78,7 @@ export default async function Page({ params }: Props) {
             {BlockRenderer(
                {
                   layout,
+                  nonce,
                   title: heroBannerComponent.items[0]?.title ?? '',
                   categories,
                   states,
--- app/page.tsx
+++ app/page.tsx
@@ -69,6 +69,7 @@ export default async function Page({ params }: Props) {
         {BlockRenderer(
             {
               layout,
+              nonce,
               title: heroBannerComponent.items[0]?.title ?? '',
               categories,
               states,
